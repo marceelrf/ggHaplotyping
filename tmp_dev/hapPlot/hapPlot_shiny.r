cbfb8133988c8dae27ab15749c298a4684468c0d
Icaro <icaroscalisse@gmail.com>
2024-11-25 15:24:27 -0300

    oi gabi e marcel

--- tmp_dev/hapPlot/hapPlot_shiny.r
+++ tmp_dev/hapPlot/hapPlot_shiny.r
@@ -1,11 +1,7 @@
-# Load pacman package
 if (!requireNamespace("pacman", quietly = TRUE)) {
-  
-  install.packages("pacman",
-                   repos = "http://cran.us.r-project.org")
+  install.packages("pacman", repos = "http://cran.us.r-project.org")
 }
 library(pacman)
-
 
 # List of packages to load
 packages <- c("shiny",
@@ -99,7 +95,7 @@ processar_gerar_grafico <- function(input_file) {
     ) %>% 
     mutate(Gene = factor(Gene, levels = unique(Gene)))
   
- plot_presence <- plot_data_5_samples %>%
+  plot_presence <- plot_data_5_samples %>%
     filter(Presence == 1) %>%  # Filtra os dados para incluir apenas onde Presence == 1
     mutate(Haplo = ifelse(grepl("h1", Gene_Haplo),
                           "H1", "H2"),
@@ -116,8 +112,6 @@ processar_gerar_grafico <- function(input_file) {
                   ymax = as.numeric(Sample) + 0.01),  # Altura da caixa
               color = "black", 
               linewidth = 0.5) +
-    # geom_gene_label(aes(label = Alelo),
-    #                size = 20) +  # Ajusta o tamanho do texto no rótulo
     facet_wrap(Sample ~ Haplo, 
                scales = "free_y", 
                ncol = 1) +  # Facetar por amostra e haplótipo (H1 e H2) em linhas
@@ -133,12 +127,15 @@ processar_gerar_grafico <- function(input_file) {
           axis.text = element_blank(),
           legend.title = element_blank(),
           legend.position = "top",
-          strip.text = element_blank()) +
+          strip.text = element_blank(), 
+          plot.margin = margin(t = 5, r = 10, b = 5, l = 20)) +
     geom_label(aes(x = -1.5, 
                    label = Sample_Haplo),  # Usa a nova coluna Sample_Haplo para mostrar Sample e Haplo
-               size = 5.5, 
+               size = 3,  # Reduz o tamanho do texto
                fill = NA,  # Remove a cor de fundo da caixa do nome da amostra
-               show.legend = FALSE)  # Adiciona rótulo com o nome da amostra e haplótipo  
+               show.legend = FALSE,
+               nudge_x = 1.1)  # Ajusta a posição do texto para a direita, se necessário
+  
   
  plot_alleles <- plot_data_5_samples %>%
     filter(Presence == 1) %>%  # Filtra os dados para incluir apenas onde Presence == 1
@@ -168,83 +165,140 @@ processar_gerar_grafico <- function(input_file) {
     labs(x = "Genes", 
          y = "Sample", 
          title = "") +
-    theme(panel.grid.major = element_blank(), 
-          panel.grid.minor = element_blank(),
-          axis.title = element_blank(),
-          axis.text = element_blank(),
-          legend.title = element_blank(),
-          legend.position = "top",
-          strip.text = element_blank()) +
-    geom_label(aes(x = -1.5, 
-                   label = Sample_Haplo),  # Usa a nova coluna Sample_Haplo para mostrar Sample e Haplo
-               size = 5.5, 
-               fill = NA,  # Remove a cor de fundo da caixa do nome da amostra
-               show.legend = FALSE)  # Adiciona rótulo com o nome da amostra e haplótipo 
+   theme(panel.grid.major = element_blank(), 
+         panel.grid.minor = element_blank(),
+         axis.title = element_blank(),
+         axis.text = element_blank(),
+         legend.title = element_blank(),
+         legend.position = "top",
+         strip.text = element_blank(), 
+         plot.margin = margin(t = 5, r = 10, b = 5, l = 20)) +
+   geom_label(aes(x = -1.5, 
+                  label = Sample_Haplo),  # Usa a nova coluna Sample_Haplo para mostrar Sample e Haplo
+              size = 3,  # Reduz o tamanho do texto
+              fill = NA,  # Remove a cor de fundo da caixa do nome da amostra
+              show.legend = FALSE,
+              nudge_x = 1.1)  # Adiciona rótulo com o nome da amostra e haplótipo 
  return(plots = list(plot_presence = plot_presence,plot_alleles = plot_alleles))
 }
 
 
 ui <- fluidPage(
-  titlePanel("KIR Haplotypes App"), #nome do app
+  tags$div(
+    style = "display: flex; align-items: center; margin-bottom: 10px;",
+    tags$h1("KIR Haplotypes App", style = "margin-right: 15px;"),
+    tags$img(src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFDRAnTfp1qWUVX1BaXrlLze1pDJImh3nr7A&s", 
+             height = "75px")
+  ),
   sidebarLayout( 
-    sidebarPanel( #criar o painel do lado esquerdo
-      width = 3,  # Ajusta o tamanho do painel lateral (3 é a largura em colunas do layout Bootstrap)
-      fileInput("file1", "Choose a data file", accept = c(".txt"), width = "100%"),  # Largura do painel
+    sidebarPanel( 
+      width = 3,
+      fileInput("file1", "Upload a data file", accept = c(".txt"), width = "80%"),
       actionButton("goButton", "Generate Graph", style = "width: 100%;"),
-      tags$hr(), #add uma linha horizontal
-      selectInput("select", "Select which information you want",  #montar a caixinha para selecionar
+      tags$hr(),
+      selectInput("select", "Select which information you want",
                   choices = c("Only Presence", "With the alleles"),
-                  selected = "Only Presence"), # "Plot Presence" é a opção selecionada por padrão
-      tags$hr(), #add uma linha horizontal
-      textInput(inputId = "txt", label = "Define the title of the haplotype:", value = "KIR haplotypes (h1 and h2) for each sample") #caixinha para o usr escrever o titulo
+                  selected = "Only Presence"),
+      tags$hr(),
+      textInput(inputId = "txt", label = "Define the title of the haplotype:", value = "KIR haplotypes (h1 and h2) for each sample"),
+      tags$hr(),
+      downloadButton("savePlot", "Save Current Plot", style = "width: 100%;")
     ),
-    
-    mainPanel( #painel principal
-      h3(textOutput("txt", container = span)), #escreve o titulo
-      plotOutput("grafico", height = "800px", width = "100%")  # Tamanho do gráfico
+    mainPanel(
+      h3(textOutput("txt", container = span)),
+      plotOutput("grafico", height = "600px", width = "100%")
     )
   )
 )
 
-server <- function(input, output) 
-{
+ui <- fluidPage(
+  tags$div(
+    style = "display: flex; align-items: center; margin-bottom: 10px;",
+    tags$h1("KIR Haplotypes App", style = "margin-right: 15px;"),
+    tags$img(src = "https://www.fmb.unesp.br/Home/Pesquisa/unidadedepesquisaexperimental/dedicados/lgmb-no-site-771x460.png", height = "75px")
+  ),
+  sidebarLayout( 
+    sidebarPanel( 
+      width = 3,
+      fileInput("file1", "Upload a data file", accept = c(".txt"), width = "100%"),
+      actionButton("goButton", "Generate Graph", style = "width: 100%;"),
+      tags$hr(),
+      selectInput("select", "Select which information you want",
+                  choices = c("Only Presence", "With the alleles"),
+                  selected = "Only Presence"),
+      tags$hr(),
+      textInput(inputId = "txt", label = "Define the title of the haplotype:", value = "KIR haplotypes (h1 and h2) for each sample"),
+      tags$hr(),
+      downloadButton("savePlot", "Save Current Plot", style = "width: 100%;")
+    ),
+    mainPanel(
+      h3(textOutput("txt", container = span)),
+      plotOutput("grafico", height = "600px", width = "100%")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
   # Armazenar a lista de gráficos reativamente
   stored_plots <- reactiveVal(NULL)  # Para armazenar os gráficos
   
- # Processa o arquivo ao clicar no botão
-  observeEvent(input$goButton,
-               {
-                 req(input$file1)
-                 
-                 # Ler o arquivo
-                 file_data <- read.table(input$file1$datapath, header = TRUE, sep = "\t")
-                 plots <- processar_gerar_grafico(input$file1$datapath)
-                 stored_plots(plots)
-                 
-                 #coloca o titulo
-                 output$txt <- renderText({
-                   input$txt
-                 })
-                 
-                 # Renderiza o gráfico com base na opção selecionada
-                 output$grafico <- renderPlot({
-                   # Verifique se a lista de gráficos está vazia ou se não foi gerada
-                   plots <- stored_plots()
-                   
-                   if (is.null(plots)) {
-                     return(NULL)  # Caso não haja gráficos, não renderiza nada
-                   }
-                   
-                   # Se o usuário selecionou "With the alleles", exibe o gráfico de alelos
-                   if (input$select == "With the alleles") {
-                     return(plots$plot_alleles)  # Exibe o gráfico de alelos
-                   } else {
-                     # Caso contrário, exibe o gráfico de presença
-                     return(plots$plot_presence)  # Exibe o gráfico de presença
-                   }
-               })
-               })
+  # Processa o arquivo ao clicar no botão
+  observeEvent(input$goButton, {
+    req(input$file1)
+    
+    # Ler o arquivo
+    file_data <- read.table(input$file1$datapath, header = TRUE, sep = "\t")
+    plots <- processar_gerar_grafico(input$file1$datapath)
+    stored_plots(plots)
+    
+    # Coloca o título
+    output$txt <- renderText({
+      input$txt
+    })
+    
+    # Renderiza o gráfico com base na opção selecionada
+    output$grafico <- renderPlot({
+      # Verifique se a lista de gráficos está vazia ou se não foi gerada
+      plots <- stored_plots()
+      if (is.null(plots)) {
+        return(NULL)  # Caso não haja gráficos, não renderiza nada
+      }
+      
+      # Se o usuário selecionou "With the alleles", exibe o gráfico de alelos
+      if (input$select == "With the alleles") {
+        return(plots$plot_alleles)  # Exibe o gráfico de alelos
+      } else {
+        # Caso contrário, exibe o gráfico de presença
+        return(plots$plot_presence)  # Exibe o gráfico de presença
+      }
+    })
+  })
+  
+  # Botão para salvar o gráfico exibido atualmente
+  output$savePlot <- downloadHandler(
+    filename = function() {
+      if (input$select == "With the alleles") {
+        return("alleles_plot.png")
+      } else {
+        return("presence_plot.png")
+      }
+    },
+    content = function(file) {
+      plots <- stored_plots()
+      if (!is.null(plots)) {
+        if (input$select == "With the alleles") {
+          ggsave(file, plot = plots$plot_alleles, width = 11.5, height = 7, bg = "white")
+        } else {
+          ggsave(file, plot = plots$plot_presence, width = 11.5, height = 7, bg = "white")
+        }
+      }
+    }
+  )
 }
 
 
+
+# Rodar o app
 shinyApp(ui = ui, server = server)
+
